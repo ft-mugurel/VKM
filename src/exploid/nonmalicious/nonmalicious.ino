70784469d1ce936765ead6ab5007a81c333e50cf
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2025-07-31 05:54:11 +0300

    ADD arduino nonmalicious code

--- src/exploid/nonmalicious/nonmalicious.ino
+++ src/exploid/nonmalicious/nonmalicious.ino
@@ -14,26 +14,11 @@ void setup() {
   Serial.print("3\n");
   delay(1000);
   Serial.print("done\n");
-
-  Serial.print("reset\n");
   delay(1000);
-  Serial.print("key\n");
-  delay(1000);
-  Serial.print("vulndrv\n");
-  delay(1000);
-  Serial.print("pname\n");
-  delay(1000);
-  Serial.print("joystick\n");
-  delay(1000);
-  Serial.print("ccount\n");
-  delay(1000);
-  Serial.print("3\n");
-  delay(1000);
-  Serial.print("done\n");
 }
 
 void loop() {
   //random(1,4);
-  Serial.print("X=120,Y=80,B=1\n");
+  Serial.print("120, 80, 1\n");
   delay(1000);
 }
